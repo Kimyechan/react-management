887865ec54b58d38e9c1d8288c9ebe84487a2fd1
Kimyechan <vlvkcjswo7@naver.com>
2020-02-24 13:52:34 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,54 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
-  const customers = [
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '찹찹찹',
-      'birthday': '961022',
-      'gender': '남자',
-      'job': '프로그래머'
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '홍길밥',
-      'birthday': '971222',
-      'gender': '남자',
-      'job': '회사원'
-    }
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidh: 1080
+  }
+})
 
-  ]
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            ></Customer>
-          )
-        })
-      }
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '찹찹찹',
+    'birthday': '961022',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길밥',
+    'birthday': '971222',
+    'gender': '남자',
+    'job': '회사원'
+  }
+
+]
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper classNAme={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  ></Customer>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
