42e72519a5d528dc80e2ec2d1a9b118f254719d1
Kimyechan <vlvkcjswo7@naver.com>
2020-02-27 16:48:11 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import Customer from './components/Customer';
-// import Table from '@material-ui/core/Table';
-// import Paper from '@material-ui/core/Paper';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableRow from '@material-ui/core/TableRow';
-// import TableCell from '@material-ui/core/TableCell';
-// import { withStyles } from '@material-ui/core/styles';
-
-// const styles = theme => ({
-//   root: {
-//     width: "100%",
-//     marginTop: theme.spacing(3),
-//     overflowX: "auto"
-//   },
-//   table: {
-//     minWidh: 1080
-//   }
-// })
-
-
-// class App extends Component {
-//   state = {
-//     customers: ''
-//   }
-
-//   componentDidMount() {
-//     this.callApi()
-//       .then(res => this.setState({ customers: res }))
-//       .catch(err => console.log(err));
-//   }
-
-//   callApi = async () => {
-//     const response = await fetch('/api/customers');
-//     const body = await response.json();
-//     return body;
-//   }
-
-//   render() {
-//     const { classes } = this.props;
-//     return (
-//       <Paper className={classes.root}>
-//         <Table className={classes.table}>
-//           <TableHead>
-//             <TableCell>번호</TableCell>
-//             <TableCell>이미지</TableCell>
-//             <TableCell>이름</TableCell>
-//             <TableCell>생년월일</TableCell>
-//             <TableCell>성별</TableCell>
-//             <TableCell>직업</TableCell>
-//           </TableHead>
-//           <TableBody>
-//             {this.state.customers ? this.state.customers.map(c => {
-//               return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-//             }) : ''}
-//           </TableBody>
-//         </Table>
-//       </Paper>
-//     );
-//   }
-// }
-
-// export default withStyles(styles)(App);
-
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
@@ -73,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -83,16 +18,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
 
 class App extends Component {
 
   state = {
-    customers: ''
+    customers: '',
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
@@ -102,6 +42,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   }
 
   render() {
@@ -122,7 +67,13 @@ class App extends Component {
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => {
               return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            }) : ''}
+            }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
